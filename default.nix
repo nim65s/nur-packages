b185b266f347bc0373cc5d510aedd72083cad879
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-03-27 16:18:16 +0100

    add osgqt

--- default.nix
+++ default.nix
@@ -15,6 +15,5 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  osgqt = pkgs.libsForQt5.callPackage ./pkgs/osgqt { };
 }
