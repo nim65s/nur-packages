686286c206e381129f7db0dda0d093dafde4776a
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-03-27 16:18:35 +0100

    remove example-package

--- default.nix
+++ default.nix
@@ -14,6 +14,5 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
   osgqt = pkgs.libsForQt5.callPackage ./pkgs/osgqt { };
 }
