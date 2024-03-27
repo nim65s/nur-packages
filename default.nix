32cdf7b69b5140ccff3eba00b01273aedd84c998
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-03-27 16:37:40 +0100

    add sway-lone-titlebar

--- default.nix
+++ default.nix
@@ -7,8 +7,31 @@
 #     nix-build -A mypackage
 
 { pkgs ? import <nixpkgs> { } }:
-
-{
+let
+  sway-lone-titlebar-unwrapped = (pkgs.sway-unwrapped.overrideAttrs (
+      finalAttrs: previousAttrs: {
+        src = pkgs.fetchFromGitHub {
+          owner = "svalaskevicius";
+          repo = "sway";
+          rev = "hiding-lone-titlebar-scenegraph";
+          hash = "sha256-cXBEXWUj3n9txzpzDgl6lsNot1ag1sEE07WAwfCLWHc=";
+        };
+      }
+    )).override
+      {
+        wlroots = pkgs.wlroots.overrideAttrs {
+          version = "0.18.0-dev";
+          src = pkgs.fetchFromGitLab {
+            domain = "gitlab.freedesktop.org";
+            owner = "wlroots";
+            repo = "wlroots";
+            rev = "873e8e455892fbd6e85a8accd7e689e8e1a9c776";
+            hash = "sha256-5zX0ILonBFwAmx7NZYX9TgixDLt3wBVfgx6M24zcxMY=";
+          };
+          patches = [ ];
+        };
+      };
+in {
   # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
@@ -34,4 +57,5 @@
     inherit (pkgs) python3Packages;
   });
   qpoases = pkgs.callPackage ./pkgs/qpoases { };
+  sway-lone-titlebar = pkgs.sway.override { sway-unwrapped = sway-lone-titlebar-unwrapped; };
 }
