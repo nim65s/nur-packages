c85c6315d047f84c8e0f44c6a6bf13551e4b7a09
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-05-31 18:34:42 +0200

    multicontact-api: nix-init

---
+++ pkgs/multicontact-api/default.nix
@@ -0,0 +1,31 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+}:
+
+stdenv.mkDerivation rec {
+  pname = "multicontact-api";
+  version = "3.0.3";
+
+  src = fetchFromGitHub {
+    owner = "loco-3d";
+    repo = "multicontact-api";
+    rev = "v${version}";
+    hash = "sha256-qZ9PyF6PNAl4d7QBogyUPHZ2/F8sEVxKH2jX5TxVnc0=";
+    fetchSubmodules = true;
+  };
+
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  meta = with lib; {
+    description = "This package install a python module used to define, store and use ContactSequence objects";
+    homepage = "https://github.com/loco-3d/multicontact-api";
+    license = licenses.bsd2;
+    maintainers = with maintainers; [ nim65s ];
+    mainProgram = "multicontact-api";
+    platforms = platforms.all;
+  };
+}
