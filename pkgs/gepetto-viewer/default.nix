2852e5f619abcd2cbf85a2b3b026a8fc393ebd1c
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-06-10 20:02:46 +0200

    gepetto-viewer: use binarywrapper

--- pkgs/gepetto-viewer/default.nix
+++ pkgs/gepetto-viewer/default.nix
@@ -1,22 +1,21 @@
 {
   stdenv,
-  writeScript,
+  makeBinaryWrapper,
   lib,
+  libsForQt5,
   gepetto-viewer-base,
   gepetto-viewer-corba,
 }:
-let
-  wrapper = writeScript "gepetto-gui" ''
-    export GEPETTO_GUI_PLUGIN_DIRS=${gepetto-viewer-corba}/lib
-    ${lib.getExe gepetto-viewer-base}
-  '';
-in
 stdenv.mkDerivation {
   inherit (gepetto-viewer-base) pname version meta;
+  buildInputs = [makeBinaryWrapper];
   #propagatedBuildInputs = [ corba ];
   dontUnpack = true;
   installPhase = ''
     mkdir -p $out/bin
-    cp ${wrapper} $out/bin/gepetto-gui
+    makeBinaryWrapper ${lib.getExe gepetto-viewer-base} $out/bin/gepetto-gui \
+      --set GEPETTO_GUI_PLUGIN_DIRS ${gepetto-viewer-corba}/lib \
+      --set QP_QPA_PLATFORM_PLUGIN_PATH ${libsForQt5.qtbase.bin}/lib/qt-${libsForQt5.qtbase.version}/plugins \
+      --set QT_QPA_PLATFORM xcb
   '';
 }
