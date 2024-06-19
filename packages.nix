1dd9f7551b53367af046581000b8eea5608ef9f8
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-06-19 11:08:33 +0200

    hpp-corbaserver

--- packages.nix
+++ packages.nix
@@ -91,11 +91,17 @@ let
   hpp-constraints = pkgs.callPackage ./pkgs/hpp-constraints { inherit hpp-pinocchio hpp-statistics; };
   hpp-baxter = pkgs.callPackage ./pkgs/hpp-baxter { };
   hpp-core = pkgs.callPackage ./pkgs/hpp-core {
-    inherit hpp-constraints hpp-pinocchio hpp-statistics proxsuite;
+    inherit
+      hpp-constraints
+      hpp-pinocchio
+      hpp-statistics
+      proxsuite
+      ;
   };
   hpp-manipulation = pkgs.callPackage ./pkgs/hpp-manipulation {
     inherit hpp-core hpp-universal-robot;
   };
+  hpp-corbaserver = pkgs.callPackage ./pkgs/hpp-corbaserver { inherit hpp-core; };
 in
 {
   inherit
@@ -115,6 +121,7 @@ in
     hpp-template-corba
     hpp-pinocchio
     hpp-constraints
+    hpp-corbaserver
     hpp-baxter
     hpp-core
     hpp-manipulation
