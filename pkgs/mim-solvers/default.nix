c1b0c50ea8794283683d3d0e0710e1a05c48ad3f
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-06-26 17:02:33 +0200

    nix-init -u https://github.com/cmake-wheel/mim_solvers

---
+++ pkgs/mim-solvers/default.nix
@@ -0,0 +1,30 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+}:
+
+stdenv.mkDerivation rec {
+  pname = "mim-solvers";
+  version = "0.0.4.c0";
+
+  src = fetchFromGitHub {
+    owner = "cmake-wheel";
+    repo = "mim_solvers";
+    rev = "v${version}";
+    hash = "sha256-3lGQbEIxGfccedd7MCZfoUOM7pbjH0zMgBAXZ1jOZ78=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+  ];
+
+  meta = with lib; {
+    description = "Implementation of numerical solvers used in the Machines in Motion Laboratory";
+    homepage = "https://github.com/cmake-wheel/mim_solvers";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ nim65s ];
+    mainProgram = "mim-solvers";
+    platforms = platforms.all;
+  };
+}
