35ead205cc7f0b2357132b654be65181169ed4e5
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-10-29 19:09:31 +0100

    sway disable titlebar

--- packages.nix
+++ packages.nix
@@ -10,6 +10,16 @@
   pkgs ? import <nixpkgs> { },
 }:
 let
+  sway-disable-titlebar-unwrapped = pkgs.sway-unwrapped.overrideAttrs (
+    _final: _prev: {
+      patches = [
+        (pkgs.fetchpatch {
+          url = "https://raw.githubusercontent.com/neuromagus/disable_titlebar_in_sway/refs/heads/main/disable_titlebar_sway1-10.patch";
+          hash = "sha256-okX63A9bBPnfYFZTtcQPhyKHUCzaEqtDwT/FNFy0xOM=";
+        })
+      ];
+    }
+  );
   sway-lone-titlebar-unwrapped =
     (pkgs.sway-unwrapped.overrideAttrs (
       _final: prev: {
@@ -42,8 +52,9 @@ let
       };
 in
 {
-  inherit sway-lone-titlebar-unwrapped;
+  inherit sway-disable-titlebar-unwrapped sway-lone-titlebar-unwrapped;
 
   sauce-code-pro = pkgs.nerdfonts.override { fonts = [ "SourceCodePro" ]; };
+  sway-disable-titlebar = pkgs.sway.override { sway-unwrapped = sway-disable-titlebar-unwrapped; };
   sway-lone-titlebar = pkgs.sway.override { sway-unwrapped = sway-lone-titlebar-unwrapped; };
 }
